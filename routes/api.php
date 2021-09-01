7050102af97ad972dbcf3051b761d60470f6c354
May-Abdelghany <mayawaad622@gmail.com>
2021-09-01 05:40:05 +0200

    change to posts

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/products',[ProductsController::class,'index']);
+Route::get('/posts',[ProductsController::class,'index']);
